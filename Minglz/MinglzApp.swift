9f1ddaa833820ae87203ab53f6413dcd7bd4886f
Justin Oh <jjujjun0603@gmail.com>
2025-07-18 21:41:22 +0900

    Initial Commit

---
+++ Minglz/MinglzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinglzApp.swift
+//  Minglz
+//
+//  Created by 오승준 on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MinglzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MinglzTests/MinglzTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinglzTests.swift
+//  MinglzTests
+//
+//  Created by 오승준 on 7/18/25.
+//
+
+import Testing
+@testable import Minglz
+
+struct MinglzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
